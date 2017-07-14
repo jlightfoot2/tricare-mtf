2cffec4b1535d8dd729e3c6cf554326d11859a06
Jack Lightfoot <jack.lightfoot@tee2.org>
2017-07-13 22:46:55 -0700

    working on android

--- webpack.cordova.config.js
+++ webpack.cordova.config.js
@@ -3,7 +3,6 @@ var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin'),
     PathRewriterPlugin = require('webpack-path-rewriter')
 var CleanWebpackPlugin = require('clean-webpack-plugin');
-var OfflinePlugin = require('offline-plugin');  
 const buildPath = path.resolve(__dirname, 'cordova/www'); 
 module.exports = {
     entry: [
@@ -53,17 +52,10 @@ module.exports = {
             'NODE_ENV': JSON.stringify('production')
           },
           '__DEVTOOLS__': false,
-          '__INCLUDE_SERVICE_WORKER__': true,
           '__IS_CORDOVA_BUILD__': true
         }),
-       /*
-        new CleanWebpackPlugin(['dist'], {
-        //  root: '/full/project/path',
-        //  verbose: true,
-        //  dry: false,
-        //  exclude: ['shared.js']
-        }),
-            */
+
+
         new webpack.optimize.CommonsChunkPlugin({
           children: true, // Look for common dependencies in all children,
           minChunks: 2 // How many times a dependency must come up before being extracted
@@ -74,7 +66,6 @@ module.exports = {
 
         new webpack.NamedModulesPlugin(),
         new webpack.optimize.UglifyJsPlugin(),
-        new PathRewriterPlugin(),
-        new OfflinePlugin()
+        new PathRewriterPlugin()
     ]
 };
